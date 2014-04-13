fdb495271a576f71e5d787ebe9549198f5bcd023
jeff ward <jsward.17@gmail.com>
2014-04-13 19:19:40 -0400

    rename to CLI

--- bin/cli.js
+++ bin/cli.js
@@ -3,3 +3,8 @@ var path = require('path');
 var fs = require('fs');
 var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
 require(lib + '/index.js');
+
+
+// if(process.argv[2].length > 0){
+//   sys.puts('theres an arg');
+// }
